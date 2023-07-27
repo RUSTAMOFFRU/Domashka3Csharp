af96f84b5d34aee6e373c8e1246713905beccd59
Rustamoff <hrust24@mail.ru>
2023-07-27 23:24:24 +0300

    Сделал задания

---
+++ task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+metka: Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+    goto metka;
+}
+
+int result = ReverseInt5(num);
+if (result == num) Console.WriteLine($"Это число -> {num} - палиндром");
+else Console.WriteLine($"Это число -> {num} не палиндром");
+
+int ReverseInt5(int number)
+{
+    return  number % 10 * 10000 + (number / 10 % 10) * 1000 + (number / 100 % 10) * 100 + (number / 1000 % 10) * 10 + (number / 10000);
+}
---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+    return;
+}
+CubeTable(number);
+
+void CubeTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        double cube = Math.Pow(i, 3);
+        Console.WriteLine($"{i,3} {cube,7}");
+    }
+}
